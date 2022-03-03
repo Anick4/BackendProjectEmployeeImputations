5295f384bd555986e1ca08181939d5f08b73555f
Adrian Aguer Pintos <91671222+Anick4@users.noreply.github.com>
2022-03-03 16:44:31 +0100

    Revert "BBDD NUEVA"
    This reverts commit ed72784753a1a0626585d0efbce76fc0998a09ae.

--- BBDD/BBDDimPUTACIONES.sql
+++ BBDD/BBDDimPUTACIONES.sql
@@ -16,162 +16,158 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `calendars`
+-- Table structure for table `calendarios`
 --
 
-DROP TABLE IF EXISTS `calendars`;
+DROP TABLE IF EXISTS `calendarios`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `calendars` (
-  `calendar_id` int NOT NULL AUTO_INCREMENT,
-  `daily_hours` int NOT NULL,
-  `monday` bit(1) NOT NULL,
-  `tuesday` bit(1) NOT NULL,
-  `wednesday` bit(1) NOT NULL,
-  `thursday` bit(1) NOT NULL,
-  `friday` bit(1) NOT NULL,
-  `saturday` bit(1) NOT NULL,
-  `sunday` bit(1) NOT NULL,
-  PRIMARY KEY (`calendar_id`)
+CREATE TABLE `calendarios` (
+  `idcalendarios` int NOT NULL AUTO_INCREMENT,
+  `horas_diarias` int NOT NULL,
+  `lunes` bit(1) NOT NULL,
+  `martes` bit(1) NOT NULL,
+  `miercoles` bit(1) NOT NULL,
+  `jueves` bit(1) NOT NULL,
+  `viernes` bit(1) NOT NULL,
+  `sabado` bit(1) NOT NULL,
+  `domingo` bit(1) NOT NULL,
+  PRIMARY KEY (`idcalendarios`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `calendars`
+-- Dumping data for table `calendarios`
 --
 
-LOCK TABLES `calendars` WRITE;
-/*!40000 ALTER TABLE `calendars` DISABLE KEYS */;
-INSERT INTO `calendars` VALUES (1,8,_binary '',_binary '',_binary '',_binary '',_binary '',_binary '\0',_binary '\0'),(2,9,_binary '',_binary '',_binary '',_binary '',_binary '\0',_binary '\0',_binary '\0');
-/*!40000 ALTER TABLE `calendars` ENABLE KEYS */;
+LOCK TABLES `calendarios` WRITE;
+/*!40000 ALTER TABLE `calendarios` DISABLE KEYS */;
+INSERT INTO `calendarios` VALUES (1,8,_binary '',_binary '',_binary '',_binary '',_binary '',_binary '\0',_binary '\0'),(2,9,_binary '',_binary '',_binary '',_binary '',_binary '\0',_binary '\0',_binary '\0');
+/*!40000 ALTER TABLE `calendarios` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `employees`
+-- Table structure for table `empleados`
 --
 
-DROP TABLE IF EXISTS `employees`;
+DROP TABLE IF EXISTS `empleados`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `employees` (
-  `employee_id` int NOT NULL AUTO_INCREMENT,
-  `employee_code` varchar(45) NOT NULL,
-  `name` varchar(45) NOT NULL,
-  `surname` varchar(45) NOT NULL,
-  `email` varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
-  `calendar_id` int NOT NULL,
-  `rol_id` int NOT NULL,
-  PRIMARY KEY (`employee_id`),
-  UNIQUE KEY `employee_code_UNIQUE` (`employee_code`),
-  UNIQUE KEY `employee_id_UNIQUE` (`employee_id`),
-  KEY `fk_employees_calendars_idx` (`calendar_id`),
-  KEY `fk_employees_roles_idx` (`rol_id`) /*!80000 INVISIBLE */,
-  CONSTRAINT `fk_employees_calendars_id` FOREIGN KEY (`calendar_id`) REFERENCES `calendars` (`calendar_id`),
-  CONSTRAINT `fk_employees_roles_id` FOREIGN KEY (`rol_id`) REFERENCES `roles` (`rol_id`)
+CREATE TABLE `empleados` (
+  `EmpleadoId` int NOT NULL AUTO_INCREMENT,
+  `Codigo_empleado` varchar(45) NOT NULL,
+  `Nombre` varchar(45) NOT NULL,
+  `Apellidos` varchar(45) NOT NULL,
+  `Email` varchar(45) NOT NULL,
+  `Contraseña` varchar(45) NOT NULL,
+  `calendarios_idcalendarios` int NOT NULL,
+  `roles_idRoles1` int NOT NULL,
+  PRIMARY KEY (`EmpleadoId`,`calendarios_idcalendarios`,`roles_idRoles1`),
+  UNIQUE KEY `codigo_empleado_UNIQUE` (`Codigo_empleado`),
+  UNIQUE KEY `idempleado_UNIQUE` (`EmpleadoId`),
+  KEY `fk_empleados_calendarios1_idx` (`calendarios_idcalendarios`),
+  KEY `fk_empleados_roles1_idx` (`roles_idRoles1`),
+  CONSTRAINT `fk_empleados_calendarios1` FOREIGN KEY (`calendarios_idcalendarios`) REFERENCES `calendarios` (`idcalendarios`),
+  CONSTRAINT `fk_empleados_roles1` FOREIGN KEY (`roles_idRoles1`) REFERENCES `roles` (`idRoles`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `employees`
+-- Dumping data for table `empleados`
 --
 
-LOCK TABLES `employees` WRITE;
-/*!40000 ALTER TABLE `employees` DISABLE KEYS */;
-INSERT INTO `employees` VALUES (2,'123456','adrian','aguer','adri@hotmail.com','agentina',1,1),(3,'345435','carol','montero','carlota@hotmail.com','brownie',2,1);
-/*!40000 ALTER TABLE `employees` ENABLE KEYS */;
+LOCK TABLES `empleados` WRITE;
+/*!40000 ALTER TABLE `empleados` DISABLE KEYS */;
+INSERT INTO `empleados` VALUES (2,'123456','adrian','aguer','adri@hotmail.com','agentina',1,1),(3,'345435','carol','montero','carlota@hotmail.com','brownie',2,1);
+/*!40000 ALTER TABLE `empleados` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `employees_projects`
+-- Table structure for table `empleados_proyectos`
 --
 
-DROP TABLE IF EXISTS `employees_projects`;
+DROP TABLE IF EXISTS `empleados_proyectos`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `employees_projects` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `project_id` int NOT NULL,
-  `employee_id` int NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
-  KEY `fk_projects_has_employees_employees_idx` (`employee_id`) /*!80000 INVISIBLE */,
-  KEY `fk_projects_has_employees_projects.idx` (`project_id`) /*!80000 INVISIBLE */,
-  CONSTRAINT `fk_projects_has_employees_employees` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`employee_id`),
-  CONSTRAINT `fk_projects_has_employees_projects` FOREIGN KEY (`project_id`) REFERENCES `projects` (`project_id`)
+CREATE TABLE `empleados_proyectos` (
+  `proyectos_idproyectos` int NOT NULL,
+  `empleados_idempleados` int NOT NULL,
+  PRIMARY KEY (`proyectos_idproyectos`,`empleados_idempleados`),
+  KEY `fk_proyectos_has_empleados_empleados_idx` (`empleados_idempleados`),
+  KEY `fk_proyectos_has_empleados_proyectos_idx` (`proyectos_idproyectos`),
+  CONSTRAINT `fk_proyectos_has_empleados_empleados` FOREIGN KEY (`empleados_idempleados`) REFERENCES `empleados` (`EmpleadoId`),
+  CONSTRAINT `fk_proyectos_has_empleados_proyectos` FOREIGN KEY (`proyectos_idproyectos`) REFERENCES `proyectos` (`idproyectos`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `employees_projects`
+-- Dumping data for table `empleados_proyectos`
 --
 
-LOCK TABLES `employees_projects` WRITE;
-/*!40000 ALTER TABLE `employees_projects` DISABLE KEYS */;
-/*!40000 ALTER TABLE `employees_projects` ENABLE KEYS */;
+LOCK TABLES `empleados_proyectos` WRITE;
+/*!40000 ALTER TABLE `empleados_proyectos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `empleados_proyectos` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `imputations`
+-- Table structure for table `imputaciones`
 --
 
-DROP TABLE IF EXISTS `imputations`;
+DROP TABLE IF EXISTS `imputaciones`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `imputations` (
-  `imputation_id` int NOT NULL AUTO_INCREMENT,
-  `day` tinyint NOT NULL,
-  `hours` tinyint NOT NULL,
-  `extra_hours` tinyint NOT NULL,
-  `state` enum('sent','approved','rejected') NOT NULL,
-  `employee_id` int NOT NULL,
-  `week` int NOT NULL,
-  `project_id` int NOT NULL,
-  PRIMARY KEY (`imputation_id`),
-  UNIQUE KEY `imputations_id_UNIQUE` (`imputation_id`),
-  KEY `fk_imputations_employees_idx` (`employee_id`),
-  KEY `fk_imputations_projects_id_idx` (`project_id`),
-  CONSTRAINT `fk_imputations_employees_id` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`employee_id`),
-  CONSTRAINT `fk_imputations_projects_id` FOREIGN KEY (`project_id`) REFERENCES `projects` (`project_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
+CREATE TABLE `imputaciones` (
+  `idimputaciones` int NOT NULL AUTO_INCREMENT,
+  `dia` tinyint NOT NULL,
+  `horas` tinyint NOT NULL,
+  `estado` enum('enviado','aprobado','rechazado') NOT NULL,
+  `extras` tinyint NOT NULL,
+  `empleados_idempleados1` int NOT NULL,
+  `semana` int NOT NULL,
+  `proyectos_idproyectos` int NOT NULL,
+  PRIMARY KEY (`idimputaciones`,`empleados_idempleados1`,`proyectos_idproyectos`),
+  UNIQUE KEY `idimputaciones_UNIQUE` (`idimputaciones`),
+  KEY `fk_imputaciones_empleados1_idx` (`empleados_idempleados1`),
+  CONSTRAINT `fk_imputaciones_empleados1` FOREIGN KEY (`empleados_idempleados1`) REFERENCES `empleados` (`EmpleadoId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `imputations`
+-- Dumping data for table `imputaciones`
 --
 
-LOCK TABLES `imputations` WRITE;
-/*!40000 ALTER TABLE `imputations` DISABLE KEYS */;
-INSERT INTO `imputations` VALUES (2,1,4,4,'sent',2,3,1);
-/*!40000 ALTER TABLE `imputations` ENABLE KEYS */;
+LOCK TABLES `imputaciones` WRITE;
+/*!40000 ALTER TABLE `imputaciones` DISABLE KEYS */;
+/*!40000 ALTER TABLE `imputaciones` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `projects`
+-- Table structure for table `proyectos`
 --
 
-DROP TABLE IF EXISTS `projects`;
+DROP TABLE IF EXISTS `proyectos`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `projects` (
-  `project_id` int NOT NULL AUTO_INCREMENT,
-  `state` bit(1) NOT NULL,
-  `generic` bit(1) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `responsible_id` int NOT NULL,
-  PRIMARY KEY (`project_id`),
-  UNIQUE KEY `idproyectos_UNIQUE` (`project_id`)
+CREATE TABLE `proyectos` (
+  `idproyectos` int NOT NULL AUTO_INCREMENT,
+  `estado` bit(1) NOT NULL,
+  `generico` bit(1) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `imputaciones_idimputaciones` int DEFAULT NULL,
+  `responsable_id` int NOT NULL,
+  PRIMARY KEY (`idproyectos`),
+  KEY `fk_proyectos_imputaciones1_idx` (`imputaciones_idimputaciones`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `projects`
+-- Dumping data for table `proyectos`
 --
 
-LOCK TABLES `projects` WRITE;
-/*!40000 ALTER TABLE `projects` DISABLE KEYS */;
-INSERT INTO `projects` VALUES (1,_binary '',_binary '','apolo',1);
-/*!40000 ALTER TABLE `projects` ENABLE KEYS */;
+LOCK TABLES `proyectos` WRITE;
+/*!40000 ALTER TABLE `proyectos` DISABLE KEYS */;
+INSERT INTO `proyectos` VALUES (1,_binary '',_binary '','apolo',0,1);
+/*!40000 ALTER TABLE `proyectos` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -182,10 +178,12 @@ DROP TABLE IF EXISTS `roles`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `roles` (
-  `rol_id` int NOT NULL AUTO_INCREMENT,
-  `rol_name` varchar(45) NOT NULL,
-  PRIMARY KEY (`rol_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+  `idRoles` int NOT NULL AUTO_INCREMENT,
+  `junior` bit(1) NOT NULL,
+  `senior` bit(1) NOT NULL,
+  `manager` bit(1) NOT NULL,
+  PRIMARY KEY (`idRoles`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -194,7 +192,7 @@ CREATE TABLE `roles` (
 
 LOCK TABLES `roles` WRITE;
 /*!40000 ALTER TABLE `roles` DISABLE KEYS */;
-INSERT INTO `roles` VALUES (1,'Junior'),(2,'Senior'),(3,'Manager');
+INSERT INTO `roles` VALUES (1,_binary '',_binary '\0',_binary '\0');
 /*!40000 ALTER TABLE `roles` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -207,4 +205,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-03-03 12:28:24
+-- Dump completed on 2022-02-28 15:39:10
