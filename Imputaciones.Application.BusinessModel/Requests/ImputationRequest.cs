def7961cd77433947e571453f392866bd4eac794
Adrian Aguer Pintos <91671222+Anick4@users.noreply.github.com>
2022-03-04 10:42:16 +0100

    no message

--- Imputaciones.Application.BusinessModel/Requests/ImputationRequest.cs
+++ Imputaciones.Application.BusinessModel/Requests/ImputationRequest.cs
@@ -8,13 +8,9 @@ namespace Imputaciones.Application.BusinessModel.Requests
 {
     public class ImputationRequest
     {
-        public int Imputation_Id { get; set; }
-        public int Day { get; set; }
-        public int Hours { get; set; }
-        public string State { get; set; }
-        public int Extra_Hours { get; set; }
         public int Week { get; set; }
+        
         public int Employee_Id { get; set; }
-        public int Project_Id { get; set; }
+
     }
 }
